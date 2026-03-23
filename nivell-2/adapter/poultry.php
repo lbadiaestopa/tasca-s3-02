76400d3bb6dd0a9a474e30a3d411ea23ea37dd5a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-23 21:35:23 +0100

    feat: add TurkeyAdapter class and DuckInterface interface following adapter pattern

--- nivell-2/adapter/poultry.php
+++ nivell-2/adapter/poultry.php
@@ -1,23 +1,46 @@
 <?php
 
-class Duck {
+interface DuckInterface 
+{
+    function quack();
+    function fly();
+}
+class Duck implements DuckInterface
+{
+    public function quack() {
+        echo "Quack \n";
+    }
 
-       public function quack() {
-              echo "Quack \n";
-       }
-
-       public function fly() {
-              echo "I'm flying \n";
-       }
+    public function fly() {
+        echo "I'm flying \n";
+    }
 }
 
-class Turkey {
+class Turkey 
+{
+    public function gobble() {
+        echo "Gobble gobble \n";
+    }
 
-       public function gobble() {
-              echo "Gobble gobble \n";
-       }
-
-       public function fly() {
+    public function fly() {
        echo "I'm flying a short distance \n";
-       }
+    }
+}
+
+class TurkeyAdapter implements DuckInterface
+{
+    protected $turkey;
+
+    public function __construct(Turkey $turkey)
+    {
+        $this->turkey = $turkey;
+    }
+
+    public function quack() {
+        $this->turkey->gobble();
+    }
+
+    public function fly() {
+        for ($i = 0; $i < 5; $i++) $this->turkey->fly();
+    }
 }
