14e615552e8b635dc9f2dfbf1484338dedd1cf1a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-23 20:26:06 +0100

    add source code

---
+++ nivell-2/adapter/poultry.php
@@ -0,0 +1,23 @@
+<?php
+
+class Duck {
+
+       public function quack() {
+              echo "Quack \n";
+       }
+
+       public function fly() {
+              echo "I'm flying \n";
+       }
+}
+
+class Turkey {
+
+       public function gobble() {
+              echo "Gobble gobble \n";
+       }
+
+       public function fly() {
+       echo "I'm flying a short distance \n";
+       }
+}
