86cf8eaa6b87478ea556fcfb994f870a7238c803
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-23 21:32:57 +0100

    feat: add index for testing

---
+++ nivell-2/adapter/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/poultry.php';
+
+function duck_interaction($duck) {
+       $duck->quack();
+       $duck->fly();
+}
+
+$duck = new Duck;
+$turkey = new Turkey;
+$turkey_adapter = new TurkeyAdapter($turkey);
+
+echo "The Turkey says...\n";
+$turkey->gobble();
+$turkey->fly();
+echo "The Duck says...\n";
+duck_interaction($duck);
+echo "The TurkeyAdapter says...\n";
+duck_interaction($turkey_adapter);
