a5563dfa5eaee9e4f46162d4bcf170372f66df0c
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-24 16:49:57 +0100

    feat: add addSubmission() function

--- nivell-3/observer/src/SubjectInterface.php
+++ nivell-3/observer/src/SubjectInterface.php
@@ -4,5 +4,6 @@ interface SubjectInterface
 {
     public function attach(ObserverInterface $observer): void;
     public function detach(ObserverInterface $observer): void;
+    public function addSubmission(Submission $submission): void;
     public function notify(Submission $submission): void;
 }
