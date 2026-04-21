7571b2dbe97932491afba4e6de298fa8ce4047fb
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-21 13:21:19 +0200

    refactor: add objects directly on Person constructor

--- nivell-1/dependency-injection/index.php
+++ nivell-1/dependency-injection/index.php
@@ -6,12 +6,6 @@ require_once __DIR__ . '/Key.php';
 require_once __DIR__ . '/PublicTransportCard.php';
 require_once __DIR__ . '/Smartphone.php';
 
-$leatherWallet = new Wallet('leather');
-$houseKey = new Key('house');
-$carKey = new Key('car');
-$publicTransportCard = new PublicTransportCard();
-$smartphone = new Smartphone();
-
-$person = new Person([$leatherWallet, $houseKey, $carKey, $publicTransportCard, $smartphone]);
+$person = new Person([new Wallet('leather'), new Key('house'), new Key('car'), new PublicTransportCard(), new Smartphone()]);
 
 $person->checkObjects();
